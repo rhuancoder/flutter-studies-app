1d45572f06e59b05db56f5da7c1bb42c60fdc20f
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-05-04 20:29:36 -0300

    feat: add button

--- lib/main.dart
+++ lib/main.dart
@@ -15,49 +15,83 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Stack(
-                alignment: AlignmentDirectional.center,
-                children: [
-                  Container(
-                    color: Colors.red,
-                    width: 100,
-                    height: 100,
+        home: Container(
+          color: Colors.white,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: [
+                    Container(
+                      color: Colors.red,
+                      width: 100,
+                      height: 100,
+                    ),
+                    Container(
+                      color: Colors.blue,
+                      width: 50,
+                      height: 50,
+                    )
+                  ],
+                ),
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: [
+                    Container(
+                      color: Colors.blue,
+                      width: 100,
+                      height: 100,
+                    ),
+                    Container(
+                      color: Colors.red,
+                      width: 50,
+                      height: 50,
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: Colors.cyan,
+                      height: 50,
+                      width: 50,
+                    ),
+                    Container(
+                      color: Colors.pinkAccent,
+                      height: 50,
+                      width: 50,
+                    ),
+                    Container(
+                      color: Colors.purple,
+                      height: 50,
+                      width: 50,
+                    )
+                  ],
+                ),
+                Container(
+                  color: Colors.amber,
+                  height: 30,
+                  width: 300,
+                  child: Text(
+                    'Yellow Diamond',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 28,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  Container(
-                    color: Colors.blue,
-                    width: 50,
-                    height: 50,
-                  )
-                ],
-              ),
-              Stack(
-                alignment: AlignmentDirectional.center,
-                children: [
-                  Container(
-                    color: Colors.blue,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    color: Colors.red,
-                    width: 50,
-                    height: 50,
-                  )
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(color: Colors.cyan, height: 50, width: 50,),
-                  Container(color: Colors.pinkAccent, height: 50,width: 50,),
-                  Container(color: Colors.purple, height: 50, width: 50,)
-                ],
-              )
-            ]));
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print('The button as been pressed.');
+                  },
+                  child: Text('Press the button'),
+                )
+              ]),
+        ));
   }
 }
