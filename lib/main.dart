a3fc07c45548843af30e87a681e04e31d7d86094
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-05-04 16:46:51 -0300

    feat: add columns and rows

--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Stack(
-        alignment: AlignmentDirectional.center,
-        children: [
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-          ),
-          Container(
-            color: Colors.blue,
-            width: 50,
-            height: 50,
-          )
-        ],
-      ),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    width: 50,
+                    height: 50,
+                  )
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    color: Colors.blue,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.red,
+                    width: 50,
+                    height: 50,
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(color: Colors.cyan, height: 50, width: 50,),
+                  Container(color: Colors.pinkAccent, height: 50,width: 50,),
+                  Container(color: Colors.purple, height: 50, width: 50,)
+                ],
+              )
+            ]));
   }
 }
